4472dcc4bc5df82e2b3bb158e063e045dc898350
Nep <spica@starry.blue>
2021-04-04 23:37:21 +0900

    Better error handling on TimeshiftGochanResProvider

--- src/main/kotlin/blue/starry/saya/services/gochan/TimeshiftGochanResProvider.kt
+++ src/main/kotlin/blue/starry/saya/services/gochan/TimeshiftGochanResProvider.kt
@@ -2,11 +2,9 @@ package blue.starry.saya.services.gochan
 
 import blue.starry.saya.models.Definitions
 import blue.starry.saya.services.comments.TimeshiftCommentProviderImpl
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.flow.*
 import kotlin.time.hours
 
 class TimeshiftGochanResProvider(
@@ -18,22 +16,28 @@ class TimeshiftGochanResProvider(
 ): TimeshiftCommentProviderImpl(channel, startAt, endAt) {
     private val allowedRange = 3.hours
 
-    override suspend fun fetch() = coroutineScope {
+    override suspend fun fetch(): Unit = coroutineScope {
         comments.withLock { list ->
             list.clear()
         }
 
         boards.asFlow().flatMapConcat { board ->
             AutoPastGochanThreadSelector.enumerate(client, board, startAt, endAt, allowedRange)
-        }.map {
-            val dat = client.get2chScDat(it.list.server, it.list.board, it.id)
+        }.mapNotNull { thread ->
+            val dat = try {
+                client.get2chScDat(thread.list.server, thread.list.board, thread.id)
+            } catch (e: CancellationException) {
+                throw e
+            } catch (t: Throwable) {
+                return@mapNotNull null
+            }
 
             GochanDatParser.parse(dat).filter { res ->
                 res.time.toEpochSecond() in startAt until endAt
             }.map { res ->
                 res.toSayaComment(
-                    source = "5ch 過去ログ [${it.title}]",
-                    sourceUrl = it.url
+                    source = "5ch 過去ログ [${thread.title}]",
+                    sourceUrl = thread.url
                 )
             }
         }.toList().flatten().sortedBy {
@@ -43,7 +47,5 @@ class TimeshiftGochanResProvider(
                 list.addAll(it)
             }
         }
-
-        Unit
     }
 }
